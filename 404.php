d8ba018f1ad79f38a24a4b26ad795cbbcd2a9690
root <root@80-78-240-233.cloudvps.regruhosting.ru>
2021-10-17 01:01:54 +0300

    Добавлена 404

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
+    <section class="not-found">
+        <div class="container">
+            <img src="<?=SITE_TEMPLATE_PATH?>/assets/images/404.png" alt="">
+            <h1 class="not-found__title title-first">Запрашиваемая страница<br> не найдена</h1><a class="button" href="/">Вернуться на главную</a>
+        </div>
+    </section>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
